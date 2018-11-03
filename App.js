7d58b49ec152da6926a035cf1af5d1cce86fa8a1
Mohamed Hussein <mohamed.hussein.1.9.9.71.9.9.8@gmail.com>
2018-11-03 20:54:16 +0200

    board pic
    removed the welcome text and added the board pic

--- App.js
+++ App.js
@@ -7,47 +7,29 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Image, StatusBar, View,} from 'react-native';
 
- const instructions = Platform.select({
-   ios: 'IOS',
-   android:'Android'
- });
+//  const instructions = Platform.select({
+//    ios: 'IOS',
+//    android:'Android'
+//  });
 
 // display different things for different platforms ------------------------------------------
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello World</Text>
-        <Text style={styles.rea}>Done Using React-Native Library</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <View style={{flex:1}}>
+        <Image
+         style={{ height: null, width: null,flex:1}}
+          source={require('./src/board.jpeg')}
+          resizeMode={'stretch'}
+        />
+        <StatusBar hidden={true} />
+        
+        </View>
     );
   }
 }
 
-const styles = ({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-     color :'green'
-  },
-  rea:{
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  }
-});
+
